74e0ced24f01aaf3a0d94752e7572f07453d839c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-04-05 00:53:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://iraffle.live', 'https://irafflevip.tk', 'https://iraffletv.nl', 'https://irafflevip.lat', 'https://ferceris.boutique/'],
+  originWhitelist: ['https://iraffle.live', 'https://irafflevip.tk', 'https://iraffletv.nl', 'https://irafflevip.lat', 'https://ferceris.boutique'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
